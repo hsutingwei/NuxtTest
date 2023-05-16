3f63642d32627c66c4c0705c08705bc18fe2ac87
yves.hsu <yves.hsu@ytec.com.tw>
2023-05-16 16:11:36 +0800

    feat: New function to get google's token

--- pages/login.vue
+++ pages/login.vue
@@ -3,21 +3,35 @@
         <button type="button" @click="handleGoogleLogin">
             使用 Google 繼續
         </button>
+        <span>{{ userInfo }}</span>
     </div>
 </template>
   
 <script setup lang="ts">
-import { googleAuthCodeLogin } from 'vue3-google-login'
+import { googleTokenLogin } from 'vue3-google-login'
 
 const runtimeConfig = useRuntimeConfig()
 const { googleClientId: GOOGLE_CLIENT_ID } = runtimeConfig.public
 
+const userInfo = ref()
 
-const handleGoogleLogin = () => {
-    googleAuthCodeLogin({
+const handleGoogleLogin = async () => {
+    const accessToken = await googleTokenLogin({
         clientId: GOOGLE_CLIENT_ID
-    }).then((response) => {
-        console.log(response)
+    }).then((response) => response?.access_token)
+
+    if (!accessToken) {
+        return '登入失敗'
+    }
+
+    const { data } = await useFetch('/api/auth/google', {
+        method: 'POST',
+        body: {
+            accessToken
+        },
+        initialCache: false
     })
+
+    userInfo.value = data.value
 }
 </script>
