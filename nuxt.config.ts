3371548be2ef33b3fe99b1de62a7f2b123a0a36c
yves.hsu <yves.hsu@ytec.com.tw>
2023-05-16 14:58:23 +0800

    feat: Successfully using Plugins vue3GoogleLogin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
             AUTH0_CLIENTID: process.env.AUTH0_CLIENTID,
             AUTH0_SECRET: process.env.AUTH0_SECRET,
             DB_CON: process.env.DB_CON,
-            GOOGLE_CLIENTID: '83643837435-4d6323gjrqt0gs7fivomh2n8f858o4gs.apps.googleusercontent.com',
+            googleClientId: process.env.GOOGLE_CLIENTID,
         }
     },
     plugins: [
--- plugins/vue3-google-login.client.ts
+++ plugins/vue3-google-login.client.ts
@@ -1,11 +1,10 @@
 import vue3GoogleLogin from 'vue3-google-login';
-const env_value = useRuntimeConfig();
 
 export default defineNuxtPlugin((nuxtApp) => {
-    const runtimeConfig = useRuntimeConfig();
-    const clientid = env_value.public.GOOGLE_CLIENTID;
+    const runtimeConfig = useRuntimeConfig()
+    const { googleClientId: GOOGLE_CLIENT_ID } = runtimeConfig.public
 
     nuxtApp.vueApp.use(vue3GoogleLogin, {
-        clientId: clientid
-    });
+        clientId: GOOGLE_CLIENT_ID
+    })
 })
