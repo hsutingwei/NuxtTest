5d7a2a3b66a091c2a5413de39d963476478705b3
Yves <jjohny51207@gmail.com>
2023-05-19 00:02:03 +0800

    feat: Add offline access

--- pages/login.vue
+++ pages/login.vue
@@ -9,8 +9,8 @@
 let google_auth0_link = ref('');
 let facebook_auth0_link = ref('');
 const env_value = useRuntimeConfig();
-google_auth0_link.value = env_value.public.AUTH0_DOMAIN + '/authorize?audience=NuxtLoginAPI&scope=openid%20profile%20email&response_type=code&client_id='
+google_auth0_link.value = env_value.public.AUTH0_DOMAIN + '/authorize?audience=NuxtLoginAPI&scope=openid%20profile%20email%20offline_access&response_type=code&client_id='
     + env_value.public.AUTH0_CLIENTID + '&redirect_uri=' + env_value.public.REDIRECT_URL + '&connection=google-oauth2';
-facebook_auth0_link.value = env_value.public.AUTH0_DOMAIN + '/authorize?audience=NuxtLoginAPI&scope=openid%20profile%20email&response_type=code&client_id='
+facebook_auth0_link.value = env_value.public.AUTH0_DOMAIN + '/authorize?audience=NuxtLoginAPI&scope=openid%20profile%20email%20offline_access&response_type=code&client_id='
     + env_value.public.AUTH0_CLIENTID + '&redirect_uri=' + env_value.public.REDIRECT_URL + '&connection=facebook';
 </script>
