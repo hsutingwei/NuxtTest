28a7305c0d650c0183be27406d28a693e600de9e
Yves <jjohny51207@gmail.com>
2023-05-18 22:19:04 +0800

    feat: Move tool.ts to utils folder

--- server/utils/tool.ts
+++ server/utils/tool.ts
@@ -1,9 +1,10 @@
 import crypto from 'crypto';
 
-export function EncryptionBySHA256(oriString: string, salt: string): string {
+export function EncryptionBySHA256(oriString: string, salt: string) {
     const hashPassword = crypto
         .createHash('sha256')
         .update(oriString + salt)
         .digest('hex');
     console.log(hashPassword);
+    return hashPassword;
 }
