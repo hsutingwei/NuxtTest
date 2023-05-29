9c0f6671d2cef8d9cbeecd666ba08dcd0288fe25
yves.hsu <yves.hsu@ytec.com.tw>
2023-05-29 16:10:23 +0800

    feat: New SECRET and REFRESH_SECRET in .env

--- server/api/login.ts
+++ server/api/login.ts
@@ -1,21 +1,29 @@
-import { LoginRequest, ApiResponse } from '~/type'
-import pg from 'pg'
+import { LoginRequest, ApiResponse } from '~/type';
 import { userOP } from '../utils/userTool';
+import jwt from 'jsonwebtoken'
 
 export default defineEventHandler(async (event) => {
     const { username, password } = await readBody<LoginRequest>(event);
 
-    const uo = new userOP(username, password);
-    const isValid = await uo.accountValid();
+    const payload = {
+        user_id: username,
+    }
+    const token = jwt.sign(payload, salt_global, { expiresIn: '1 day' });
+    const decoded = jwt.verify(token, salt_global);
+    console.log('=======');
+    console.log(decoded);
+
+    /*const uo = new userOP(username, password);
+    const isValid = await uo.accountValid();*/
     let success = false;
-    if (isValid) {
+    /*if (isValid) {
         const keyResponse = await uo.getLoginKey();
         console.log(keyResponse);
         if (keyResponse.success) {
             setCookie(event, 'userid', keyResponse.data.publicKey);
             success = true;
         }
-    }
+    }*/
 
     return {
         success: success
